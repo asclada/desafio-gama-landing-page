916c70b42697cd42010158942b899d66425fdbb5
Lucas Santana <asclada@gmail.com>
2021-07-18 23:54:14 -0300

    first commit

---
+++ localstorage.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    let btnSave = document.getElementById(btnSaveName);
+    let btnRead = document.getElementById(btnSaveEmail); 
+
+    btnSave.onclick = saveName;
+    btnRead.onclick = saveEmail;
+}
+
+function saveName() {
+    let inputName = document.getElementById("");
+}
+
+function saveEmail() {
+
+}
